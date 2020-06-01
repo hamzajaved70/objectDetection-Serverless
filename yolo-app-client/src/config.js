463407d15fe6cda44dec237a472061bf0e8844b9
Hamza Javed <hjav0001@student.monash.edu>
2020-06-02 07:36:41 +1000

    Login and Sign Up

---
+++ yolo-app-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "YOUR_S3_UPLOADS_BUCKET_REGION",
+        BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
+    },
+    apiGateway: {
+        REGION: "YOUR_API_GATEWAY_REGION",
+        URL: "YOUR_API_GATEWAY_URL"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_KMFNwzLjB",
+        APP_CLIENT_ID: "3f1idmfm3kppcjfe2c16io3trh",
+        IDENTITY_POOL_ID: "us-east-1:e9dc4179-d5f0-4bfd-a767-e1db71aa694d"
+    }
+};
