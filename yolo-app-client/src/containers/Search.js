459f95e10c60f88127f51d7c29d5a700b17242cf
Farhad-Rezwan <frez0003@student.monash.edu>
2020-06-11 18:23:01 +1000

    Network error

--- yolo-app-client/src/containers/Search.js
+++ yolo-app-client/src/containers/Search.js
@@ -1,14 +1,55 @@
 import React, { Component } from "react";
 import "./Search.css";
+import axios from 'axios';
+
+
+
 export default class Search extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            tags : '',
+          };
+        }
+
+    changeHandler = (e) => {
+        this.setState({[e.target.name]: e.target.value.split(' ')})
+      }
+
+    submitHandler = e => {
+        e.preventDefault()
+        console.log(this.state);
+        axios.post('https://aeamk3bczc.execute-api.us-east-1.amazonaws.com/query/api/fetchimages/', this.state)
+            .then(response => {
+                console.log(response)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+
+          }
+
+
     render() {
+
+        const { tags } = this.state
         return (
-            <div className="Search">
-                <div className="lander">
-                    <h1>Find Query</h1>
-                    <p>Please enter your query</p>
-                </div>
-            </div>
-        );
+          <div>
+              <form onSubmit= { this.submitHandler }>
+                  <div>
+                    <input
+                        type="text"
+                        name="tags"
+                        value={tags}
+                        onChange= { this.changeHandler }/>
+
+                  </div>
+                  <button type="submit"> submit </button>
+
+              </form>
+
+          </div>
+        )
     }
 }
